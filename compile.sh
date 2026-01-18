b810c97bf3615c4d807ab4510f4c52bc4654cae9
Leandro Cañellas <federico.leandro.c@gmail.com>
2026-01-17 23:30:07 -0300

    Actualiza automatización de compilación/debug para utilizar ddd con -ddd.

--- compile.sh
+++ compile.sh
@@ -3,13 +3,17 @@
 # $1: [-d|--run|-c]
 # --run: Run the compiled file
 # -d:    Starts gdb debugging session
+# -ddd:  Start ddd debugging session
 # -c:    Compile the program
 # Run: ./compile.sh [-d|--run] exercise-folder/file.s
 
 # Flags
 f_debug=0
+f_ddd_debug=0
 f_run=0
 f_compile=0
+
+flags_on=0
 
 #region Analyse parameters
 if [[ $# -eq 0 ]]; then
@@ -17,12 +21,21 @@ if [[ $# -eq 0 ]]; then
     exit 1
 fi
 
-[[ "$@" =~ "--run" ]] && f_run=1
-[[ "$@" =~ "-d" ]] && f_debug=1
-[[ "$@" =~ "-c" ]] && f_compile=1
+[[ "$@" =~ "--run" ]] && ((f_run = 1))
+[[ "$@" =~ "-ddd" ]] && ((f_ddd_debug = 1))
+[[ $f_ddd_debug -eq 0 && "$@" =~ "-d" ]] && ((f_debug = 1))
+[[ "$@" =~ "-c" ]] && ((f_compile = 1))
 
-if [[ $f_debug -eq 1 && $f_run -eq 1 && $f_compile -eq 1 ]]; then
-    echo "Error: Can't run and debug at the same time!"
+# Count active flags
+flags_on=$((f_run + f_ddd_debug + f_debug + f_compile))
+
+if (( (f_debug && f_ddd_debug) || \
+      (f_debug && f_run) || \
+      (f_debug && f_compile) || \
+      (f_compile && f_run) || \
+      (flags_on == 4) \
+)); then
+    echo "Error: Can't do this operations at same time!"
     exit 1
 fi
 
@@ -161,18 +174,18 @@ function do_compile {
 clean_file "$obj" > /dev/null
 clean_file "$bin" > /dev/null
 
-# Check that at least one of -d, --run or -c was given
-if [[ $f_run -eq 0 && $f_debug -eq 0 && $f_compile -eq 0 ]]; then
-    echo "Error: -d,--run or -c not given!"
+# Check that at least one flag was given
+if (( flags_on == 0 )); then
+    echo "Error: You must provide at least one of -d, -ddd, --run, or -c!"
     exit 1
 fi
 
 # Assemble the program
-if [[ $f_run -eq 1 || $f_compile -eq 1 ]]; then
+if (( f_run == 1 || f_compile == 1 )); then
     do_assemble "$src" "$obj" "$arch"
     do_link "$obj" "$bin"
 # Compile for debugging
-elif [[ $f_debug -eq 1 ]]; then
+elif (( f_debug == 1 || f_ddd_debug == 1 )); then
     do_compile "$src" "$bin" "$arch"
 fi
 
@@ -190,10 +203,10 @@ if [[ ! -e "$bin" ]]; then
 fi
 
 # Run when the --run is detected
-if [[ $f_run -eq 1 ]]; then
+if (( f_run == 1 )); then
     ./"$bin"
 # Debug when the -d is detected
-elif [[ $f_debug -eq 1 ]]; then
+elif (( f_debug == 1 )); then
     # Check if gdb is installed
     if [[ -z $(which gdb) ]]; then
         echo "Error: gdb is not installed!"
@@ -207,4 +220,15 @@ elif [[ $f_debug -eq 1 ]]; then
         -ex "set disassembly-flavor intel" \
         ./"${bin_file}"
     cd ..
+# Debug with ddd
+elif (( f_ddd_debug == 1 )); then
+    # Check if ddd is installed
+    if [[ -z $(which ddd) ]]; then
+        echo "Error: ddd is not installed!"
+        exit 1
+    fi
+
+    bin_file="${bin##*/}"
+    cd $exercise_folder
+    ddd ./"${bin_file}"
 fi
